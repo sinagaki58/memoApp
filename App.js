855297f1af3e2f2db73a3eee3b4e5775d9705b2c
shinagak <shinagak@yahoo-corp.jp>
2018-09-09 21:20:07 +0900

    section9

--- App.js
+++ App.js
@@ -34,8 +34,8 @@ export default class App extends React.Component {
           </View>
         </View>
 
-        <View>
-          <Text>+</Text>
+        <View style={styles.memoAddButton}>
+          <Text style={styles.memoAddButtonTitle}>+</Text>
         </View>
       </View>
     );
@@ -45,9 +45,10 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffdf6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
   },
 
   appbar: {
@@ -60,10 +61,59 @@ const styles = StyleSheet.create({
     backgroundColor: '#265366',
     justifyContent: 'center',
     alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10,
   },
 
   appbarTitle: {
     color: '#fff',
     fontSize: 18,
+  },
+
+  memoList: {
+    flex: 1,
+    width: '100%',
+  },
+
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff',
+  },
+
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#e31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+
+  memoAddButtonTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff',
   },
 });
