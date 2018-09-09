834e297b8776a078b7de54244dc4b16d61417d39
sinagaki58 <s.inagaki58@gmail.com>
2018-09-09 20:14:22 +0900

    add

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello!</Text>
+        <View style={styles.appbar}>
+          <View>
+            <Text style={styles.appbarTitle}>MEMOT</Text>
+          </View>
+        </View>
+
+        <View style={styles.memoList}>
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のタイトル</Text>
+            <Text style={styles.memoDate}>2018/09/01</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のタイトル</Text>
+            <Text style={styles.memoDate}>2018/09/01</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のタイトル</Text>
+            <Text style={styles.memoDate}>2018/09/01</Text>
+          </View>
+
+          <View style={styles.memoListItem}>
+            <Text style={styles.memoTitle}>講座のタイトル</Text>
+            <Text style={styles.memoDate}>2018/09/01</Text>
+          </View>
+        </View>
+
+        <View>
+          <Text>+</Text>
+        </View>
       </View>
     );
   }
@@ -17,5 +48,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    left: 0,
+    height: 80,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
