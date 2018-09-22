58342fc041c4c08512db2c4808a1f000eb6788b1
shinagak <shinagak@yahoo-corp.jp>
2018-09-22 23:00:47 +0900

    add

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -7,17 +7,17 @@ import {
   TouchableHighlight,
 } from 'react-native';
 
-class LoginScreen extends React.Component {
+class SignupScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.title}>
-          ログイン
+          メンバー登録
         </Text>
         <TextInput style={styles.input} value="Email address" />
         <TextInput style={styles.input} value="password" />
         <TouchableHighlight style={styles.button} onPress={() => {}} underlayColor="#c70f66">
-          <Text style={styles.buttonTitle}>ログインする</Text>
+          <Text style={styles.buttonTitle}>送信する</Text>
         </TouchableHighlight>
       </View>
     );
@@ -59,4 +59,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default LoginScreen;
+export default SignupScreen;
